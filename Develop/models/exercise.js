49c0f96ceb169107fb1904b6de188e30df712446
noracurcio <noracurcio@gmail.com>
2020-11-15 12:22:46 -0700

    added exercise model.

--- Develop/models/exercise.js
+++ Develop/models/exercise.js
@@ -2,3 +2,20 @@
 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    day: {
+        type: Date
+    },
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ]
+})
+
+
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
+
+module.exports = Exercise;
